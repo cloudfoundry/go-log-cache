df30d856f3c5083c71ad65f5c22ac120c86a6d75
Andrew Poydence <apoydence@pivotal.io>
2017-12-14 16:39:35 -0700

    Adds ingress client
    This ingress client has a Read method that makes the requests
    over the RESTful API.

---
+++ ingress_client.go
@@ -0,0 +1,88 @@
+package logcache
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	"code.cloudfoundry.org/go-log-cache/rpc/logcache"
+	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
+	"github.com/golang/protobuf/jsonpb"
+)
+
+// IngressClient reads from LogCache via the RESTful API.
+type IngressClient struct {
+	addr string
+}
+
+// NewIngressClient creates a IngressClient.
+func NewIngressClient(addr string) *IngressClient {
+	return &IngressClient{
+		addr: addr,
+	}
+}
+
+// Read queries the LogCache and returns the given envelopes. To override any
+// query defaults (e.g., end time), use the according option.
+func (c *IngressClient) Read(sourceID string, start time.Time, opts ...ReadOption) ([]*loggregator_v2.Envelope, error) {
+	u, err := url.Parse(c.addr)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = "v1/read/" + sourceID
+	q := u.Query()
+	q.Set("start_time", strconv.FormatInt(start.UnixNano(), 10))
+
+	// allow the given options to configure the URL.
+	for _, o := range opts {
+		o(u, q)
+	}
+	u.RawQuery = q.Encode()
+
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+
+	var r logcache.ReadResponse
+	if err := jsonpb.Unmarshal(resp.Body, &r); err != nil {
+		return nil, err
+	}
+
+	return r.Envelopes.Batch, nil
+}
+
+// ReadOption configures the URL that is used to submit the query. The
+// RawQuery is set to the decoded query parameters after each option is
+// invoked.
+type ReadOption func(u *url.URL, q url.Values)
+
+// WithEndTime sets the 'end_time' query parameter to the given time. It
+// defaults to empty, and therefore the end of the cache.
+func WithEndTime(t time.Time) ReadOption {
+	return func(u *url.URL, q url.Values) {
+		q.Set("end_time", strconv.FormatInt(t.UnixNano(), 10))
+	}
+}
+
+// WithLimit sets the 'limit' query parameter to the given value. It
+// defaults to empty, and therefore 100 envelopes.
+func WithLimit(limit int) ReadOption {
+	return func(u *url.URL, q url.Values) {
+		q.Set("limit", strconv.Itoa(limit))
+	}
+}
+
+// WithEnvelopeType sets the 'envelope_type' query parameter to the given value. It
+// defaults to empty, and therefore any envelope type.
+func WithEnvelopeType(t logcache.EnvelopeTypes) ReadOption {
+	return func(u *url.URL, q url.Values) {
+		q.Set("envelope_type", t.String())
+	}
+}
---
+++ ingress_client_test.go
@@ -0,0 +1,180 @@
+package logcache_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	logcache "code.cloudfoundry.org/go-log-cache"
+	rpc "code.cloudfoundry.org/go-log-cache/rpc/logcache"
+)
+
+func TestIngressClientRead(t *testing.T) {
+	t.Parallel()
+	logCache := newStubLogCache()
+	client := logcache.NewIngressClient(logCache.addr())
+
+	envelopes, err := client.Read("some-id", time.Unix(0, 99))
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if len(envelopes) != 2 {
+		t.Fatalf("expected to receive 2 envlopes, got %d", len(envelopes))
+	}
+
+	if envelopes[0].Timestamp != 99 || envelopes[1].Timestamp != 100 {
+		t.Fatal("wrong envelopes")
+	}
+
+	if len(logCache.reqs) != 1 {
+		t.Fatalf("expected have 1 request, have %d", len(logCache.reqs))
+	}
+
+	if logCache.reqs[0].URL.Path != "/v1/read/some-id" {
+		t.Fatalf("expected Path '/v1/read/some-id' but got '%s'", logCache.reqs[0].URL.Path)
+	}
+
+	if logCache.reqs[0].URL.Query().Get("start_time") != "99" {
+		t.Fatalf("expected query parameter 'start_time' to equal '99', but got '%s'", logCache.reqs[0].URL.Query().Get("start_time"))
+	}
+
+	if len(logCache.reqs[0].URL.Query()) != 1 {
+		t.Fatalf("expected only a single query parameter, but got %d", len(logCache.reqs[0].URL.Query()))
+	}
+}
+
+func TestIngressClientReadWithOptions(t *testing.T) {
+	t.Parallel()
+	logCache := newStubLogCache()
+	client := logcache.NewIngressClient(logCache.addr())
+
+	_, err := client.Read(
+		"some-id",
+		time.Unix(0, 99),
+		logcache.WithEndTime(time.Unix(0, 101)),
+		logcache.WithLimit(103),
+		logcache.WithEnvelopeType(rpc.EnvelopeTypes_LOG),
+	)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if len(logCache.reqs) != 1 {
+		t.Fatalf("expected have 1 request, have %d", len(logCache.reqs))
+	}
+
+	if logCache.reqs[0].URL.Path != "/v1/read/some-id" {
+		t.Fatalf("expected Path '/v1/read/some-id' but got '%s'", logCache.reqs[0].URL.Path)
+	}
+
+	if logCache.reqs[0].URL.Query().Get("start_time") != "99" {
+		t.Fatalf("expected query parameter 'start_time' to equal '99', but got '%s'", logCache.reqs[0].URL.Query().Get("start_time"))
+	}
+
+	if logCache.reqs[0].URL.Query().Get("end_time") != "101" {
+		t.Fatalf("expected query parameter 'end_time' to equal '101', but got '%s'", logCache.reqs[0].URL.Query().Get("end_time"))
+	}
+
+	if logCache.reqs[0].URL.Query().Get("limit") != "103" {
+		t.Fatalf("expected query parameter 'limit' to equal '103', but got '%s'", logCache.reqs[0].URL.Query().Get("limit"))
+	}
+
+	if logCache.reqs[0].URL.Query().Get("envelope_type") != "LOG" {
+		t.Fatalf("expected query parameter 'envelope_type' to equal 'LOG', but got '%s'", logCache.reqs[0].URL.Query().Get("envelope_type"))
+	}
+
+	if len(logCache.reqs[0].URL.Query()) != 4 {
+		t.Fatalf("expected only 4 query parameters, but got %d", len(logCache.reqs[0].URL.Query()))
+	}
+}
+
+func TestIngressClientReadNon200(t *testing.T) {
+	t.Parallel()
+	logCache := newStubLogCache()
+	logCache.statusCode = 500
+	client := logcache.NewIngressClient(logCache.addr())
+
+	_, err := client.Read("some-id", time.Unix(0, 99))
+
+	if err == nil {
+		t.Fatal("expected an error")
+	}
+}
+
+func TestIngressClientReadInvalidResponse(t *testing.T) {
+	t.Parallel()
+	logCache := newStubLogCache()
+	logCache.result = []byte("invalid")
+	client := logcache.NewIngressClient(logCache.addr())
+
+	_, err := client.Read("some-id", time.Unix(0, 99))
+
+	if err == nil {
+		t.Fatal("expected an error")
+	}
+}
+
+func TestIngressClientReadUnknownAddr(t *testing.T) {
+	t.Parallel()
+	client := logcache.NewIngressClient("http://invalid.url")
+
+	_, err := client.Read("some-id", time.Unix(0, 99))
+
+	if err == nil {
+		t.Fatal("expected an error")
+	}
+}
+
+func TestIngressClientReadInvalidAddr(t *testing.T) {
+	t.Parallel()
+	client := logcache.NewIngressClient("-:-invalid")
+
+	_, err := client.Read("some-id", time.Unix(0, 99))
+
+	if err == nil {
+		t.Fatal("expected an error")
+	}
+}
+
+type stubLogCache struct {
+	statusCode int
+	server     *httptest.Server
+	reqs       []*http.Request
+	result     []byte
+}
+
+func newStubLogCache() *stubLogCache {
+	s := &stubLogCache{
+		statusCode: http.StatusOK,
+		result: []byte(`{
+		"envelopes": {
+			"batch": [
+			    {
+					"timestamp": 99,
+					"sourceId": "some-id"
+				},
+			    {
+					"timestamp": 100,
+					"sourceId": "some-id"
+				}
+			]
+		}
+	}`),
+	}
+	s.server = httptest.NewServer(s)
+	return s
+}
+
+func (s *stubLogCache) addr() string {
+	return s.server.URL
+}
+
+func (s *stubLogCache) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.reqs = append(s.reqs, r)
+	w.WriteHeader(s.statusCode)
+	w.Write(s.result)
+}
