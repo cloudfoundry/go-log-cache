a1b83e925a7b5c2f7476dd8b700a5c608e74ed73
Andrew Poydence <apoydence@pivotal.io>
2017-12-15 06:26:55 -0700

    Adds Walk function
    The walk function traverses either the entire cache
    or a section of it. It handles moving the start time
    to read all the data.

---
+++ walk.go
@@ -0,0 +1,134 @@
+package logcache
+
+import (
+	"io/ioutil"
+	"log"
+	"time"
+
+	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
+)
+
+// Reader reads envelopes from LogCache. It will be invoked by Walker several
+// time to traverse the length of the cache.
+type Reader func(
+	sourceID string,
+	start time.Time,
+	opts ...ReadOption,
+) ([]*loggregator_v2.Envelope, error)
+
+// Visitor is invoked for each envelope batch. If the function returns false,
+// it doesn't make any more requests. Otherwise it reaches out for the next
+// batch of envelopes.
+type Visitor func([]*loggregator_v2.Envelope) bool
+
+// Walk reads from the LogCache until the Visitor returns false.
+func Walk(sourceID string, v Visitor, r Reader, opts ...WalkOption) {
+	c := &walkConfig{
+		log:     log.New(ioutil.Discard, "", 0),
+		backoff: AlwaysDoneBackoff{},
+	}
+
+	for _, o := range opts {
+		o.configure(c)
+	}
+
+	var readOpts []ReadOption
+	if !c.end.IsZero() {
+		readOpts = append(readOpts, WithEndTime(c.end))
+	}
+
+	for {
+		es, err := r(sourceID, time.Unix(0, c.start), readOpts...)
+		if err != nil {
+			c.log.Print(err)
+			if !c.backoff.OnErr(err) {
+				return
+			}
+			continue
+		}
+
+		if len(es) == 0 {
+			if !c.backoff.OnEmpty() {
+				return
+			}
+			continue
+		}
+
+		// If visitor is done or the next timestamp would be outside of our
+		// window (only when end is set), then be done.
+		if !v(es) || (!c.end.IsZero() && es[len(es)-1].Timestamp+1 >= c.end.UnixNano()) {
+			return
+		}
+
+		c.start = es[len(es)-1].Timestamp + 1
+	}
+}
+
+// WalkOption overrides defaults for Walk.
+type WalkOption interface {
+	configure(*walkConfig)
+}
+
+// WithWalkLogger is used to set the logger for the Walk. It defaults to
+// not logging.
+func WithWalkLogger(l *log.Logger) WalkOption {
+	return walkOptionFunc(func(c *walkConfig) {
+		c.log = l
+	})
+}
+
+// WithWalkEndTime sets the end time of the query. Once reached, Walk will
+// exit.
+func WithWalkEndTime(t time.Time) WalkOption {
+	return walkOptionFunc(func(c *walkConfig) {
+		c.end = t
+	})
+}
+
+// WithWalkBackoff sets the backoff strategy for an empty batch or error. It
+// defaults to stopping on an error or empty batch via AlwaysDoneBackoff.
+func WithWalkBackoff(b Backoff) WalkOption {
+	return walkOptionFunc(func(c *walkConfig) {
+		c.backoff = b
+	})
+}
+
+// Backoff is used to determine what to do if there is an empty batch or
+// error. If there is an error, it will be passed to the method OnErr. If there is
+// not an error and just an empty batch, the method OnEmpty will be invoked. If
+// the either method returns false, then Walk exits. On a successful read that
+// has envelopes, Reset will be invoked.
+type Backoff interface {
+	OnErr(error) bool
+	OnEmpty() bool
+	Reset()
+}
+
+// AlwaysDoneBackoff returns false for both OnErr and OnEmpty.
+type AlwaysDoneBackoff struct{}
+
+// OnErr implements Backoff.
+func (b AlwaysDoneBackoff) OnErr(error) bool {
+	return false
+}
+
+// OnEmpty implements Backoff.
+func (b AlwaysDoneBackoff) OnEmpty() bool {
+	return false
+}
+
+// Reset implements Backoff.
+func (b AlwaysDoneBackoff) Reset() {}
+
+type walkOptionFunc func(*walkConfig)
+
+func (f walkOptionFunc) configure(c *walkConfig) {
+	f(c)
+}
+
+type walkConfig struct {
+	log     *log.Logger
+	backoff Backoff
+	start   int64
+	end     time.Time
+}
---
+++ walk_test.go
@@ -0,0 +1,204 @@
+package logcache_test
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+	"time"
+
+	"code.cloudfoundry.org/go-log-cache"
+	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
+)
+
+func TestWalk(t *testing.T) {
+	t.Parallel()
+
+	r := &stubReader{}
+	logcache.Walk("some-id", func([]*loggregator_v2.Envelope) bool { return false }, r.read)
+
+	if len(r.sourceIDs) != 1 {
+		t.Fatal("expected read to be invoked once")
+	}
+
+	if r.sourceIDs[0] != "some-id" {
+		t.Fatalf("expected sourceID to equal 'some-id': %s", r.sourceIDs[0])
+	}
+
+	if r.starts[0] != 0 {
+		t.Fatalf("expected start to equal '0': %d", r.starts[0])
+	}
+}
+
+func TestWalkUsesEndTime(t *testing.T) {
+	t.Parallel()
+
+	r := &stubReader{
+		envelopes: [][]*loggregator_v2.Envelope{
+			{
+				{Timestamp: 1},
+				{Timestamp: 2},
+			},
+			{
+				{Timestamp: 3},
+				{Timestamp: 4},
+				{Timestamp: 5},
+			},
+		},
+		errs: []error{nil, nil},
+	}
+	expected := make([][]*loggregator_v2.Envelope, len(r.envelopes))
+	copy(expected, r.envelopes)
+
+	var es [][]*loggregator_v2.Envelope
+	logcache.Walk("some-id", func(b []*loggregator_v2.Envelope) bool {
+		es = append(es, b)
+		return true
+	},
+		r.read)
+
+	if len(r.sourceIDs) != 3 {
+		t.Fatalf("expected read to be invoked 3 times: %d", len(r.sourceIDs))
+	}
+
+	if !reflect.DeepEqual(r.sourceIDs, []string{"some-id", "some-id", "some-id"}) {
+		t.Fatalf("wrong sourceIDs': %v", r.sourceIDs)
+	}
+
+	if !reflect.DeepEqual(r.starts, []int64{0, 3, 6}) {
+		t.Fatalf("wrong starts: %v", r.starts)
+	}
+
+	if !reflect.DeepEqual(expected, es) {
+		t.Fatalf("wrong envelopes: %v || %v", es, expected)
+	}
+}
+
+func TestWalkWithinWindow(t *testing.T) {
+	t.Parallel()
+
+	r := &stubReader{
+		envelopes: [][]*loggregator_v2.Envelope{
+			{
+				{Timestamp: 1},
+				{Timestamp: 2},
+			},
+			{
+				{Timestamp: 3},
+			},
+			{
+				{Timestamp: 4},
+			},
+		},
+		errs: []error{nil, nil, nil},
+	}
+
+	var es []*loggregator_v2.Envelope
+	logcache.Walk("some-id", func(b []*loggregator_v2.Envelope) bool {
+		es = append(es, b...)
+		return true
+	},
+		r.read,
+		logcache.WithWalkEndTime(time.Unix(0, 4)),
+	)
+
+	if len(r.sourceIDs) != 2 {
+		t.Fatalf("expected read to be invoked 2 times: %d", len(r.sourceIDs))
+	}
+
+	if len(r.opts[0]) != 1 {
+		t.Fatal("expected EndTime option to be set")
+	}
+
+	if len(es) != 3 {
+		t.Fatalf("expected 3 envlopes: %d", len(es))
+	}
+
+	for i := 1; i < 4; i++ {
+		if es[i-1].Timestamp != int64(i) {
+			t.Fatalf("expected timestamp to equal %d: %d", i, es[i-1].Timestamp)
+		}
+	}
+}
+
+func TestWalkRetriesOnError(t *testing.T) {
+	t.Parallel()
+
+	r := &stubReader{
+		envelopes: [][]*loggregator_v2.Envelope{nil, {{Timestamp: 1}}},
+		errs:      []error{errors.New("some-error"), nil},
+	}
+	b := &stubBackoff{
+		onErrReturn: true,
+	}
+
+	var called int
+	logcache.Walk(
+		"some-id",
+		func(b []*loggregator_v2.Envelope) bool {
+			called++
+			return false
+		},
+		r.read,
+		logcache.WithWalkBackoff(b),
+	)
+
+	if len(r.sourceIDs) != 2 {
+		t.Fatalf("expected read to be invoked 2 times: %d", len(r.sourceIDs))
+	}
+
+	if called != 1 {
+		t.Fatalf("expected visit to be invoked 1 time: %d", called)
+	}
+
+	if len(b.errs) != 1 {
+		t.Fatalf("expected backoff to be invoked 1 time: %d", len(b.errs))
+	}
+}
+
+type stubBackoff struct {
+	errs          []error
+	onErrReturn   bool
+	onEmptyReturn bool
+}
+
+func (s *stubBackoff) OnErr(err error) bool {
+	s.errs = append(s.errs, err)
+	return s.onErrReturn
+}
+
+func (s *stubBackoff) OnEmpty() bool {
+	return s.onEmptyReturn
+}
+
+func (s *stubBackoff) Reset() {
+}
+
+type stubReader struct {
+	sourceIDs []string
+	starts    []int64
+	opts      [][]logcache.ReadOption
+
+	envelopes [][]*loggregator_v2.Envelope
+	errs      []error
+}
+
+func (s *stubReader) read(sourceID string, start time.Time, opts ...logcache.ReadOption) ([]*loggregator_v2.Envelope, error) {
+	s.sourceIDs = append(s.sourceIDs, sourceID)
+	s.starts = append(s.starts, start.UnixNano())
+	s.opts = append(s.opts, opts)
+
+	if len(s.envelopes) != len(s.errs) {
+		panic("envelopes and errs should have same len")
+	}
+
+	if len(s.envelopes) == 0 {
+		return nil, nil
+	}
+
+	defer func() {
+		s.envelopes = s.envelopes[1:]
+		s.errs = s.errs[1:]
+	}()
+
+	return s.envelopes[0], s.errs[0]
+}
