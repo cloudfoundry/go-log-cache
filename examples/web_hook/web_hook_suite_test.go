ab3fd33935ecba74989dac6ccf1e9f015d101e66
Travis Patterson <tpatterson@pivotal.io>
2018-02-27 12:33:23 -0700

    Add web hook example
    - moved from log-cache
    
    [#155474064]
    
    Signed-off-by: Peter Stone <pstone@pivotal.io>

---
+++ examples/web_hook/web_hook_suite_test.go
@@ -0,0 +1,13 @@
+package logcache_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestWebHook(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "WebHook Suite")
+}
