9fc290901678d3c475b00fd15f53c806d9204014
Todd Persen <tpersen@pivotal.io>
2018-08-28 14:51:07 -0600

    Add client support for PromQL range queries
    [#158271766]
    
    Signed-off-by: Mark Gangl <mgangl@pivotal.io>

--- rpc/logcache_v1/promql.pb.gw.go
+++ rpc/logcache_v1/promql.pb.gw.go
@@ -45,6 +45,23 @@ func request_PromQLQuerier_InstantQuery_0(ctx context.Context, marshaler runtime
 
 }
 
+var (
+	filter_PromQLQuerier_RangeQuery_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_PromQLQuerier_RangeQuery_0(ctx context.Context, marshaler runtime.Marshaler, client PromQLQuerierClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq PromQL_RangeQueryRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_PromQLQuerier_RangeQuery_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.RangeQuery(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterPromQLQuerierHandlerFromEndpoint is same as RegisterPromQLQuerierHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterPromQLQuerierHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -112,13 +129,46 @@ func RegisterPromQLQuerierHandlerClient(ctx context.Context, mux *runtime.ServeM
 
 	})
 
+	mux.Handle("GET", pattern_PromQLQuerier_RangeQuery_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_PromQLQuerier_RangeQuery_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PromQLQuerier_RangeQuery_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_PromQLQuerier_InstantQuery_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "promql"}, ""))
+
+	pattern_PromQLQuerier_RangeQuery_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "promql_range"}, ""))
 )
 
 var (
 	forward_PromQLQuerier_InstantQuery_0 = runtime.ForwardResponseMessage
+
+	forward_PromQLQuerier_RangeQuery_0 = runtime.ForwardResponseMessage
 )
