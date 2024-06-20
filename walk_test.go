077b509ac760aea08857950b655d375bea19a567
Carson Long <12767276+ctlong@users.noreply.github.com>
2024-06-20 11:21:26 -0700

    Bump go-loggregator to v10 outside rpc

--- walk_test.go
+++ walk_test.go
@@ -11,7 +11,7 @@ import (
 	client "code.cloudfoundry.org/go-log-cache/v3"
 
 	rpc "code.cloudfoundry.org/go-log-cache/v3/rpc/logcache_v1"
-	"code.cloudfoundry.org/go-loggregator/v9/rpc/loggregator_v2"
+	"code.cloudfoundry.org/go-loggregator/v10/rpc/loggregator_v2"
 )
 
 // Ensure client.Reader is fulfilled by Client.Read
